76087a0d721cf21b21bb98116e7400ad7c812121
database64128 <free122448@hotmail.com>
2023-03-15 16:03:44 +0800

    🎋 conn: move sockaddr functions to separate files and support other platforms

---
+++ conn/sockaddr_linuxsolaris.go
@@ -0,0 +1,54 @@
+//go:build linux || solaris
+
+package conn
+
+import (
+	"net/netip"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
+
+func AddrPortToSockaddrValue(addrPort netip.AddrPort) (rsa6 unix.RawSockaddrInet6, namelen uint32) {
+	addr, port := addrPort.Addr(), addrPort.Port()
+	p := (*[2]byte)(unsafe.Pointer(&rsa6.Port))
+	p[0] = byte(port >> 8)
+	p[1] = byte(port)
+	if addr.Is4() {
+		rsa6.Family = unix.AF_INET
+		a := (*[4]byte)(unsafe.Pointer(&rsa6.Flowinfo))
+		*a = addr.As4()
+		namelen = unix.SizeofSockaddrInet4
+		return
+	}
+	rsa6.Family = unix.AF_INET6
+	rsa6.Addr = addr.As16()
+	namelen = unix.SizeofSockaddrInet6
+	return
+}
+
+func AddrPortToSockaddrInet4(addrPort netip.AddrPort) unix.RawSockaddrInet4 {
+	addr := addrPort.Addr()
+	port := addrPort.Port()
+	rsa4 := unix.RawSockaddrInet4{
+		Family: unix.AF_INET,
+		Addr:   addr.As4(),
+	}
+	p := (*[2]byte)(unsafe.Pointer(&rsa4.Port))
+	p[0] = byte(port >> 8)
+	p[1] = byte(port)
+	return rsa4
+}
+
+func AddrPortToSockaddrInet6(addrPort netip.AddrPort) unix.RawSockaddrInet6 {
+	addr := addrPort.Addr()
+	port := addrPort.Port()
+	rsa6 := unix.RawSockaddrInet6{
+		Family: unix.AF_INET6,
+		Addr:   addr.As16(),
+	}
+	p := (*[2]byte)(unsafe.Pointer(&rsa6.Port))
+	p[0] = byte(port >> 8)
+	p[1] = byte(port)
+	return rsa6
+}
---
+++ conn/sockaddr_notwindows.go
@@ -0,0 +1,81 @@
+//go:build aix || darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris || zos
+
+package conn
+
+import (
+	"fmt"
+	"net/netip"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
+
+func SockaddrValueToAddrPort(rsa6 unix.RawSockaddrInet6, namelen uint32) (netip.AddrPort, error) {
+	p := (*[2]byte)(unsafe.Pointer(&rsa6.Port))
+	port := uint16(p[0])<<8 + uint16(p[1])
+	var addr netip.Addr
+	switch namelen {
+	case unix.SizeofSockaddrInet4:
+		addr = netip.AddrFrom4(*(*[4]byte)(unsafe.Pointer(&rsa6.Flowinfo)))
+	case unix.SizeofSockaddrInet6:
+		addr = netip.AddrFrom16(rsa6.Addr)
+	default:
+		return netip.AddrPort{}, fmt.Errorf("bad sockaddr length: %d", namelen)
+	}
+	return netip.AddrPortFrom(addr, port), nil
+}
+
+func AddrPortToSockaddr(addrPort netip.AddrPort) (name *byte, namelen uint32) {
+	if addrPort.Addr().Is4() {
+		rsa4 := AddrPortToSockaddrInet4(addrPort)
+		name = (*byte)(unsafe.Pointer(&rsa4))
+		namelen = unix.SizeofSockaddrInet4
+	} else {
+		rsa6 := AddrPortToSockaddrInet6(addrPort)
+		name = (*byte)(unsafe.Pointer(&rsa6))
+		namelen = unix.SizeofSockaddrInet6
+	}
+	return
+}
+
+func AddrPortUnmappedToSockaddr(addrPort netip.AddrPort) (name *byte, namelen uint32) {
+	if addr := addrPort.Addr(); addr.Is4() || addr.Is4In6() {
+		rsa4 := AddrPortToSockaddrInet4(addrPort)
+		name = (*byte)(unsafe.Pointer(&rsa4))
+		namelen = unix.SizeofSockaddrInet4
+	} else {
+		rsa6 := AddrPortToSockaddrInet6(addrPort)
+		name = (*byte)(unsafe.Pointer(&rsa6))
+		namelen = unix.SizeofSockaddrInet6
+	}
+	return
+}
+
+func SockaddrToAddrPort(name *byte, namelen uint32) (netip.AddrPort, error) {
+	switch namelen {
+	case unix.SizeofSockaddrInet4:
+		rsa4 := (*unix.RawSockaddrInet4)(unsafe.Pointer(name))
+		return SockaddrInet4ToAddrPort(rsa4), nil
+
+	case unix.SizeofSockaddrInet6:
+		rsa6 := (*unix.RawSockaddrInet6)(unsafe.Pointer(name))
+		return SockaddrInet6ToAddrPort(rsa6), nil
+
+	default:
+		return netip.AddrPort{}, fmt.Errorf("bad sockaddr length: %d", namelen)
+	}
+}
+
+func SockaddrInet4ToAddrPort(sa *unix.RawSockaddrInet4) netip.AddrPort {
+	portp := (*[2]byte)(unsafe.Pointer(&sa.Port))
+	port := uint16(portp[0])<<8 + uint16(portp[1])
+	ip := netip.AddrFrom4(sa.Addr)
+	return netip.AddrPortFrom(ip, port)
+}
+
+func SockaddrInet6ToAddrPort(sa *unix.RawSockaddrInet6) netip.AddrPort {
+	portp := (*[2]byte)(unsafe.Pointer(&sa.Port))
+	port := uint16(portp[0])<<8 + uint16(portp[1])
+	ip := netip.AddrFrom16(sa.Addr)
+	return netip.AddrPortFrom(ip, port)
+}
