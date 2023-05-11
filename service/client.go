bf08ccaeda71f29f2f2379f911665f355191a5de
database64128 <free122448@hotmail.com>
2023-05-12 00:15:13 +0800

    🌱 service: allow clients to have different addresses for TCP and UDP
    Now that we have server support for multiple listeners, it's only reasonable when clients can also connect to different addresses for TCP and UDP.

--- service/client.go
+++ service/client.go
@@ -15,27 +15,50 @@ import (
 // ClientConfig stores a client configuration.
 // It may be marshaled as or unmarshaled from JSON.
 type ClientConfig struct {
-	Name               string    `json:"name"`
-	Endpoint           conn.Addr `json:"endpoint"`
-	Protocol           string    `json:"protocol"`
-	DialerFwmark       int       `json:"dialerFwmark"`
-	DialerTrafficClass int       `json:"dialerTrafficClass"`
+	// Name is the name of the client.
+	Name string `json:"name"`
+
+	// Protocol is the protocol used by the client.
+	// Valid values include "direct", "socks5", "http", "none", "plain", "2022-blake3-aes-128-gcm", "2022-blake3-aes-256-gcm".
+	Protocol string `json:"protocol"`
+
+	// Endpoint is the address of the remote proxy server, if applicable.
+	//
+	// Do not use if either TCPAddress or UDPAddress is specified.
+	Endpoint conn.Addr `json:"endpoint"`
+
+	// TCPAddress is the TCP address of the remote proxy server, if applicable.
+	//
+	// Do not use if Endpoint is specified.
+	TCPAddress conn.Addr `json:"tcpAddress"`
+
+	// UDPAddress is the UDP address of the remote proxy server, if applicable.
+	//
+	// Do not use if Endpoint is specified.
+	UDPAddress conn.Addr `json:"udpAddress"`
+
+	DialerFwmark       int `json:"dialerFwmark"`
+	DialerTrafficClass int `json:"dialerTrafficClass"`
 
 	// TCP
+
 	EnableTCP bool `json:"enableTCP"`
 	DialerTFO bool `json:"dialerTFO"`
 
 	// UDP
+
 	EnableUDP bool `json:"enableUDP"`
 	MTU       int  `json:"mtu"`
 
 	// Shadowsocks
+
 	PSK           []byte   `json:"psk"`
 	IPSKs         [][]byte `json:"iPSKs"`
 	PaddingPolicy string   `json:"paddingPolicy"`
 	cipherConfig  *ss2022.ClientCipherConfig
 
 	// Taint
+
 	UnsafeRequestStreamPrefix  []byte `json:"unsafeRequestStreamPrefix"`
 	UnsafeResponseStreamPrefix []byte `json:"unsafeResponseStreamPrefix"`
 
@@ -44,28 +67,57 @@ type ClientConfig struct {
 	logger            *zap.Logger
 }
 
+func (cc *ClientConfig) checkAddresses() error {
+	if cc.Protocol == "direct" {
+		return nil
+	}
+
+	ev := cc.Endpoint.IsValid()
+	tv := cc.TCPAddress.IsValid()
+	uv := cc.UDPAddress.IsValid()
+
+	if ev == (tv || uv) {
+		return errors.New("missing or conflicting proxy server address(es)")
+	}
+
+	if ev {
+		cc.TCPAddress = cc.Endpoint
+		cc.UDPAddress = cc.Endpoint
+		return nil
+	}
+
+	if cc.EnableTCP && !tv {
+		return errors.New("missing proxy server TCP address")
+	}
+
+	if cc.EnableUDP && !uv {
+		return errors.New("missing proxy server UDP address")
+	}
+
+	return nil
+}
+
 // Initialize initializes the client configuration.
-func (cc *ClientConfig) Initialize(listenConfigCache conn.ListenConfigCache, dialerCache conn.DialerCache, logger *zap.Logger) error {
-	if cc.Protocol != "direct" && !cc.Endpoint.IsValid() {
-		return errors.New("endpoint address is required")
+func (cc *ClientConfig) Initialize(listenConfigCache conn.ListenConfigCache, dialerCache conn.DialerCache, logger *zap.Logger) (err error) {
+	if err = cc.checkAddresses(); err != nil {
+		return
 	}
 
 	switch cc.Protocol {
 	case "2022-blake3-aes-128-gcm", "2022-blake3-aes-256-gcm":
-		err := ss2022.CheckPSKLength(cc.Protocol, cc.PSK, cc.IPSKs)
-		if err != nil {
-			return err
+		if err = ss2022.CheckPSKLength(cc.Protocol, cc.PSK, cc.IPSKs); err != nil {
+			return
 		}
 		cc.cipherConfig, err = ss2022.NewClientCipherConfig(cc.PSK, cc.IPSKs, cc.EnableUDP)
 		if err != nil {
-			return err
+			return
 		}
 	}
 
 	cc.listenConfigCache = listenConfigCache
 	cc.dialerCache = dialerCache
 	cc.logger = logger
-	return nil
+	return
 }
 
 // TCPClient creates a zerocopy.TCPClient from the ClientConfig.
@@ -84,16 +136,16 @@ func (cc *ClientConfig) TCPClient() (zerocopy.TCPClient, error) {
 	case "direct":
 		return direct.NewTCPClient(cc.Name, dialer), nil
 	case "none", "plain":
-		return direct.NewShadowsocksNoneTCPClient(cc.Name, cc.Endpoint.String(), dialer), nil
+		return direct.NewShadowsocksNoneTCPClient(cc.Name, cc.TCPAddress.String(), dialer), nil
 	case "socks5":
-		return direct.NewSocks5TCPClient(cc.Name, cc.Endpoint.String(), dialer), nil
+		return direct.NewSocks5TCPClient(cc.Name, cc.TCPAddress.String(), dialer), nil
 	case "http":
-		return http.NewProxyClient(cc.Name, cc.Endpoint.String(), dialer), nil
+		return http.NewProxyClient(cc.Name, cc.TCPAddress.String(), dialer), nil
 	case "2022-blake3-aes-128-gcm", "2022-blake3-aes-256-gcm":
 		if len(cc.UnsafeRequestStreamPrefix) != 0 || len(cc.UnsafeResponseStreamPrefix) != 0 {
 			cc.logger.Warn("Unsafe stream prefix taints the client", zap.String("client", cc.Name))
 		}
-		return ss2022.NewTCPClient(cc.Name, cc.Endpoint.String(), dialer, cc.cipherConfig, cc.UnsafeRequestStreamPrefix, cc.UnsafeResponseStreamPrefix), nil
+		return ss2022.NewTCPClient(cc.Name, cc.TCPAddress.String(), dialer, cc.cipherConfig, cc.UnsafeRequestStreamPrefix, cc.UnsafeResponseStreamPrefix), nil
 	default:
 		return nil, fmt.Errorf("unknown protocol: %s", cc.Protocol)
 	}
@@ -118,20 +170,20 @@ func (cc *ClientConfig) UDPClient() (zerocopy.UDPClient, error) {
 	case "direct":
 		return direct.NewDirectUDPClient(cc.Name, cc.MTU, listenConfig), nil
 	case "none", "plain":
-		return direct.NewShadowsocksNoneUDPClient(cc.Endpoint, cc.Name, cc.MTU, listenConfig), nil
+		return direct.NewShadowsocksNoneUDPClient(cc.UDPAddress, cc.Name, cc.MTU, listenConfig), nil
 	case "socks5":
 		dialer := cc.dialerCache.Get(conn.DialerSocketOptions{
 			Fwmark:       cc.DialerFwmark,
 			TrafficClass: cc.DialerTrafficClass,
 			TCPFastOpen:  cc.DialerTFO,
 		})
-		return direct.NewSocks5UDPClient(cc.logger, cc.Name, cc.Endpoint.String(), dialer, cc.MTU, listenConfig), nil
+		return direct.NewSocks5UDPClient(cc.logger, cc.Name, cc.UDPAddress.String(), dialer, cc.MTU, listenConfig), nil
 	case "2022-blake3-aes-128-gcm", "2022-blake3-aes-256-gcm":
 		shouldPad, err := ss2022.ParsePaddingPolicy(cc.PaddingPolicy)
 		if err != nil {
 			return nil, err
 		}
-		return ss2022.NewUDPClient(cc.Endpoint, cc.Name, cc.MTU, listenConfig, cc.cipherConfig, shouldPad), nil
+		return ss2022.NewUDPClient(cc.UDPAddress, cc.Name, cc.MTU, listenConfig, cc.cipherConfig, shouldPad), nil
 	default:
 		return nil, fmt.Errorf("unknown protocol: %s", cc.Protocol)
 	}
