7e181876fc51046903d78039dd06eb1ed22f184f
database64128 <free122448@hotmail.com>
2024-12-03 16:26:55 +0800

    🍞 direct, socks5: add tests to cover auth and error cases

--- direct/stream_test.go
+++ direct/stream_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/database64128/shadowsocks-go/conn"
 	"github.com/database64128/shadowsocks-go/pipe"
+	"github.com/database64128/shadowsocks-go/socks5"
 	"github.com/database64128/shadowsocks-go/zerocopy"
 )
 
@@ -34,6 +35,7 @@ func testShadowsocksNoneStreamReadWriter(t *testing.T, ctx context.Context, clie
 	serverInitialPayload := make([]byte, len(clientInitialPayload))
 
 	var (
+		wg               sync.WaitGroup
 		c                *DirectStreamReadWriter
 		s                *DirectStreamReadWriter
 		serverTargetAddr conn.Addr
@@ -41,35 +43,37 @@ func testShadowsocksNoneStreamReadWriter(t *testing.T, ctx context.Context, clie
 		cerr, serr       error
 	)
 
-	var wg sync.WaitGroup
 	wg.Add(2)
 
 	go func() {
+		defer wg.Done()
 		c, _, cerr = NewShadowsocksNoneStreamClientReadWriter(ctx, &plo, clientTargetAddr, clientInitialPayload)
-		wg.Done()
 	}()
 
 	go func() {
+		defer wg.Done()
 		s, serverTargetAddr, serr = NewShadowsocksNoneStreamServerReadWriter(pr)
-		if len(serverInitialPayload) > 0 && serr == nil {
+		if serr != nil {
+			return
+		}
+		if len(serverInitialPayload) > 0 {
 			nr, serr = s.ReadZeroCopy(serverInitialPayload, 0, len(serverInitialPayload))
 		}
-		wg.Done()
 	}()
 
 	wg.Wait()
+
 	if cerr != nil {
 		t.Fatal(cerr)
 	}
 	if serr != nil {
 		t.Fatal(serr)
 	}
-
 	if nr != len(serverInitialPayload) {
 		t.Fatalf("Expected server initial payload bytes %d, got %d", len(serverInitialPayload), nr)
 	}
 	if !clientTargetAddr.Equals(serverTargetAddr) {
-		t.Errorf("Target address mismatch: c: %s, s: %s", clientTargetAddr, serverTargetAddr)
+		t.Errorf("Target address mismatch: c: %q, s: %q", clientTargetAddr, serverTargetAddr)
 	}
 	if !bytes.Equal(clientInitialPayload, serverInitialPayload) {
 		t.Error("Initial payload mismatch")
@@ -86,45 +90,121 @@ func TestShadowsocksNoneStreamReadWriter(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	testShadowsocksNoneStreamReadWriter(t, ctx, nil)
-	testShadowsocksNoneStreamReadWriter(t, ctx, initialPayload)
+	t.Run("NoInitialPayload", func(t *testing.T) {
+		testShadowsocksNoneStreamReadWriter(t, ctx, nil)
+	})
+
+	t.Run("WithInitialPayload", func(t *testing.T) {
+		testShadowsocksNoneStreamReadWriter(t, ctx, initialPayload)
+	})
 }
 
 func TestSocks5StreamReadWriter(t *testing.T) {
+	b := make([]byte, 255+255)
+	if _, err := rand.Read(b); err != nil {
+		t.Fatal(err)
+	}
+	userInfo255 := socks5.UserInfo{
+		Username: string(b[:255]),
+		Password: string(b[255:]),
+	}
+
+	for _, c := range []struct {
+		name                     string
+		clientAuthMsg            []byte
+		serverUserInfoByUsername map[string]socks5.UserInfo
+		expectedUsername         string
+	}{
+		{
+			name:                     "NoAuth",
+			clientAuthMsg:            nil,
+			serverUserInfoByUsername: nil,
+			expectedUsername:         "",
+		},
+		{
+			name:          "UserPassAuth/1",
+			clientAuthMsg: []byte{socks5.UsernamePasswordAuthVersion, 1, 'h', 1, 'w'},
+			serverUserInfoByUsername: map[string]socks5.UserInfo{
+				"h": {
+					Username: "h",
+					Password: "w",
+				},
+			},
+			expectedUsername: "h",
+		},
+		{
+			name:          "UserPassAuth/5",
+			clientAuthMsg: []byte{socks5.UsernamePasswordAuthVersion, 5, 'h', 'e', 'l', 'l', 'o', 5, 'w', 'o', 'r', 'l', 'd'},
+			serverUserInfoByUsername: map[string]socks5.UserInfo{
+				"hello": {
+					Username: "hello",
+					Password: "world",
+				},
+			},
+			expectedUsername: "hello",
+		},
+		{
+			name:          "UserPassAuth/255",
+			clientAuthMsg: userInfo255.AppendAuthMsg(nil),
+			serverUserInfoByUsername: map[string]socks5.UserInfo{
+				userInfo255.Username: userInfo255,
+			},
+			expectedUsername: userInfo255.Username,
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			testSocks5StreamReadWriter(t, c.clientAuthMsg, c.serverUserInfoByUsername, c.expectedUsername)
+		})
+	}
+}
+
+func testSocks5StreamReadWriter(t *testing.T, clientAuthMsg []byte, serverUserInfoByUsername map[string]socks5.UserInfo, expectedUsername string) {
 	pl, pr := pipe.NewDuplexPipe()
 
 	clientTargetAddr := conn.AddrFromIPPort(netip.AddrPortFrom(netip.IPv6Unspecified(), 53))
 
 	var (
+		wg               sync.WaitGroup
 		c                *DirectStreamReadWriter
 		s                *DirectStreamReadWriter
 		serverTargetAddr conn.Addr
+		username         string
 		cerr, serr       error
 	)
 
-	var wg sync.WaitGroup
 	wg.Add(2)
 
 	go func() {
-		c, cerr = NewSocks5StreamClientReadWriter(pl, clientTargetAddr)
-		wg.Done()
+		defer wg.Done()
+		if len(clientAuthMsg) == 0 {
+			c, cerr = NewSocks5StreamClientReadWriter(pl, clientTargetAddr)
+		} else {
+			c, cerr = NewSocks5AuthStreamClientReadWriter(pl, clientAuthMsg, clientTargetAddr)
+		}
 	}()
 
 	go func() {
-		s, serverTargetAddr, serr = NewSocks5StreamServerReadWriter(pr, true, false)
-		wg.Done()
+		defer wg.Done()
+		if serverUserInfoByUsername == nil {
+			s, serverTargetAddr, serr = NewSocks5StreamServerReadWriter(pr, true, false)
+		} else {
+			s, serverTargetAddr, username, serr = NewSocks5AuthStreamServerReadWriter(pr, serverUserInfoByUsername, true, false)
+		}
 	}()
 
 	wg.Wait()
+
 	if cerr != nil {
 		t.Fatal(cerr)
 	}
 	if serr != nil {
 		t.Fatal(serr)
 	}
-
 	if !clientTargetAddr.Equals(serverTargetAddr) {
-		t.Errorf("Target address mismatch: c: %s, s: %s", clientTargetAddr, serverTargetAddr)
+		t.Errorf("Target address mismatch: c: %q, s: %q", clientTargetAddr, serverTargetAddr)
+	}
+	if username != expectedUsername {
+		t.Errorf("username = %q, want %q", username, expectedUsername)
 	}
 
 	zerocopy.ReadWriterTestFunc(t, c, s)
---
+++ socks5/stream_test.go
@@ -0,0 +1,570 @@
+package socks5
+
+import (
+	"bytes"
+	"errors"
+	"net/netip"
+	"sync"
+	"testing"
+
+	"github.com/database64128/shadowsocks-go/conn"
+	"github.com/database64128/shadowsocks-go/pipe"
+	"github.com/database64128/shadowsocks-go/zerocopy"
+)
+
+func TestStreamClientError(t *testing.T) {
+	for _, c := range []struct {
+		name             string
+		clientAuthMsg    []byte
+		expectedRequests [][]byte
+		serverMsgs       [][]byte
+		checkClientErr   func(*testing.T, error)
+	}{
+		{
+			name:          "UnsupportedVersion/4",
+			clientAuthMsg: nil,
+			expectedRequests: [][]byte{
+				{Version, 1, MethodNoAuthenticationRequired},
+			},
+			serverMsgs: [][]byte{
+				{4, 1},
+			},
+			checkClientErr: func(t *testing.T, err error) {
+				var e UnsupportedVersionError
+				if !errors.As(err, &e) {
+					t.Fatalf("err = %v, want %T", err, e)
+				}
+				if e != 4 {
+					t.Errorf("e = %d, want 4", e)
+				}
+			},
+		},
+		{
+			name:          "UnsupportedAuthMethod",
+			clientAuthMsg: nil,
+			expectedRequests: [][]byte{
+				{Version, 1, MethodNoAuthenticationRequired},
+			},
+			serverMsgs: [][]byte{
+				{Version, MethodNoAcceptable},
+			},
+			checkClientErr: func(t *testing.T, err error) {
+				var e UnsupportedAuthMethodError
+				if !errors.As(err, &e) {
+					t.Fatalf("err = %v, want %T", err, e)
+				}
+				if e != MethodNoAcceptable {
+					t.Errorf("e = %d, want %d", e, MethodNoAcceptable)
+				}
+			},
+		},
+		{
+			name:          "UnsupportedUsernamePasswordAuthVersion",
+			clientAuthMsg: []byte{UsernamePasswordAuthVersion, 5, 'h', 'e', 'l', 'l', 'o', 5, 'w', 'o', 'r', 'l', 'd'},
+			expectedRequests: [][]byte{
+				{Version, 1, MethodUsernamePassword},
+				{UsernamePasswordAuthVersion, 5, 'h', 'e', 'l', 'l', 'o', 5, 'w', 'o', 'r', 'l', 'd'},
+			},
+			serverMsgs: [][]byte{
+				{Version, MethodUsernamePassword},
+				{4, 1},
+			},
+			checkClientErr: func(t *testing.T, err error) {
+				var e UnsupportedUsernamePasswordAuthVersionError
+				if !errors.As(err, &e) {
+					t.Fatalf("err = %v, want %T", err, e)
+				}
+				if e != 4 {
+					t.Errorf("e = %d, want 4", e)
+				}
+			},
+		},
+		{
+			name:          "IncorrectUsernamePassword",
+			clientAuthMsg: []byte{UsernamePasswordAuthVersion, 5, 'h', 'e', 'l', 'l', 'o', 5, 'w', 'o', 'r', 'l', 'd'},
+			expectedRequests: [][]byte{
+				{Version, 1, MethodUsernamePassword},
+				{UsernamePasswordAuthVersion, 5, 'h', 'e', 'l', 'l', 'o', 5, 'w', 'o', 'r', 'l', 'd'},
+			},
+			serverMsgs: [][]byte{
+				{Version, MethodUsernamePassword},
+				{UsernamePasswordAuthVersion, 1},
+			},
+			checkClientErr: func(t *testing.T, err error) {
+				if err != ErrIncorrectUsernamePassword {
+					t.Errorf("err = %v, want %v", err, ErrIncorrectUsernamePassword)
+				}
+			},
+		},
+		{
+			name:          "ReplyError",
+			clientAuthMsg: nil,
+			expectedRequests: [][]byte{
+				{Version, 1, MethodNoAuthenticationRequired},
+				{Version, CmdConnect, 0, AtypIPv6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 53},
+			},
+			serverMsgs: [][]byte{
+				{Version, MethodNoAuthenticationRequired},
+				{Version, ReplyConnectionRefused, 0, AtypIPv4, 0},
+			},
+			checkClientErr: func(t *testing.T, err error) {
+				var e ReplyError
+				if !errors.As(err, &e) {
+					t.Fatalf("err = %v, want %T", err, e)
+				}
+				if e != ReplyConnectionRefused {
+					t.Errorf("e.Reply = %d, want %d", e, ReplyConnectionRefused)
+				}
+			},
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			testStreamClientError(t, c.clientAuthMsg, c.expectedRequests, c.serverMsgs, c.checkClientErr)
+		})
+	}
+}
+
+func testStreamClientError(
+	t *testing.T,
+	clientAuthMsg []byte,
+	expectedRequests [][]byte,
+	serverMsgs [][]byte,
+	checkClientErr func(*testing.T, error),
+) {
+	pl, pr := pipe.NewDuplexPipe()
+
+	clientTargetAddr := conn.AddrFromIPPort(netip.AddrPortFrom(netip.IPv6Unspecified(), 53))
+
+	var (
+		wg         sync.WaitGroup
+		cerr, serr error
+	)
+
+	wg.Add(3)
+
+	go func() {
+		defer func() {
+			_ = pl.Close()
+			wg.Done()
+		}()
+
+		if len(clientAuthMsg) == 0 {
+			cerr = ClientConnect(pl, clientTargetAddr)
+		} else {
+			cerr = ClientConnectUsernamePassword(pl, clientAuthMsg, clientTargetAddr)
+		}
+	}()
+
+	go func() {
+		defer func() {
+			_ = pr.CloseRead()
+			wg.Done()
+		}()
+
+		b := make([]byte, 1024)
+
+		for _, expectedReq := range expectedRequests {
+			n, err := pr.Read(b)
+			if err != nil {
+				t.Errorf("Failed to read request: %v", err)
+				return
+			}
+
+			req := b[:n]
+			if !bytes.Equal(req, expectedReq) {
+				t.Errorf("req = %v, want %v", req, expectedReq)
+			}
+		}
+	}()
+
+	go func() {
+		defer func() {
+			_ = pr.CloseWrite()
+			wg.Done()
+		}()
+
+		for _, msg := range serverMsgs {
+			if _, serr = pr.Write(msg); serr != nil {
+				return
+			}
+		}
+	}()
+
+	wg.Wait()
+
+	if serr != nil {
+		t.Fatalf("serr = %v", serr)
+	}
+	checkClientErr(t, cerr)
+}
+
+func TestStreamServerError(t *testing.T) {
+	for _, c := range []struct {
+		name                     string
+		clientMsgs               [][]byte
+		expectedResponses        [][]byte
+		serverUserInfoByUsername map[string]UserInfo
+		serverEnableTCP          bool
+		serverEnableUDP          bool
+		checkServerErr           func(*testing.T, error)
+	}{
+		{
+			name: "UnsupportedVersion/4",
+			clientMsgs: [][]byte{
+				{4, 1, MethodNoAuthenticationRequired},
+			},
+			expectedResponses:        nil,
+			serverUserInfoByUsername: nil,
+			serverEnableTCP:          true,
+			serverEnableUDP:          false,
+			checkServerErr: func(t *testing.T, err error) {
+				var e UnsupportedVersionError
+				if !errors.As(err, &e) {
+					t.Fatalf("err = %v, want %T", err, e)
+				}
+				if e != 4 {
+					t.Errorf("e = %d, want 4", e)
+				}
+			},
+		},
+		{
+			name: "ZeroNMETHODS",
+			clientMsgs: [][]byte{
+				{Version, 0, MethodNoAuthenticationRequired},
+			},
+			expectedResponses:        nil,
+			serverUserInfoByUsername: nil,
+			serverEnableTCP:          true,
+			serverEnableUDP:          false,
+			checkServerErr: func(t *testing.T, err error) {
+				if err != errZeroNMETHODS {
+					t.Errorf("err = %v, want %v", err, errZeroNMETHODS)
+				}
+			},
+		},
+		{
+			name: "NoAcceptableAuthMethod/RejectClientNoAuth",
+			clientMsgs: [][]byte{
+				{Version, 1, MethodNoAuthenticationRequired},
+			},
+			expectedResponses: [][]byte{
+				{Version, MethodNoAcceptable},
+			},
+			serverUserInfoByUsername: map[string]UserInfo{
+				"hello": {
+					Username: "hello",
+					Password: "world",
+				},
+			},
+			serverEnableTCP: true,
+			serverEnableUDP: false,
+			checkServerErr: func(t *testing.T, err error) {
+				if err != ErrNoAcceptableAuthMethod {
+					t.Errorf("err = %v, want %v", err, ErrNoAcceptableAuthMethod)
+				}
+			},
+		},
+		{
+			name: "NoAcceptableAuthMethod/RejectClientUsernamePassword",
+			clientMsgs: [][]byte{
+				{Version, 1, MethodUsernamePassword},
+			},
+			expectedResponses: [][]byte{
+				{Version, MethodNoAcceptable},
+			},
+			serverUserInfoByUsername: nil,
+			serverEnableTCP:          true,
+			serverEnableUDP:          false,
+			checkServerErr: func(t *testing.T, err error) {
+				if err != ErrNoAcceptableAuthMethod {
+					t.Errorf("err = %v, want %v", err, ErrNoAcceptableAuthMethod)
+				}
+			},
+		},
+		{
+			name: "UnsupportedUsernamePasswordAuthVersion",
+			clientMsgs: [][]byte{
+				{Version, 2, MethodNoAuthenticationRequired, MethodUsernamePassword},
+				{5, 5, 'h', 'e'},
+			},
+			expectedResponses: [][]byte{
+				{Version, MethodUsernamePassword},
+			},
+			serverUserInfoByUsername: map[string]UserInfo{
+				"hello": {
+					Username: "hello",
+					Password: "world",
+				},
+			},
+			serverEnableTCP: true,
+			serverEnableUDP: false,
+			checkServerErr: func(t *testing.T, err error) {
+				var e UnsupportedUsernamePasswordAuthVersionError
+				if !errors.As(err, &e) {
+					t.Fatalf("err = %v, want %T", err, e)
+				}
+				if e != 5 {
+					t.Errorf("e = %d, want 5", e)
+				}
+			},
+		},
+		{
+			name: "ZeroULEN",
+			clientMsgs: [][]byte{
+				{Version, 1, MethodUsernamePassword},
+				{UsernamePasswordAuthVersion, 0, 0, 0},
+			},
+			expectedResponses: [][]byte{
+				{Version, MethodUsernamePassword},
+			},
+			serverUserInfoByUsername: map[string]UserInfo{
+				"hello": {
+					Username: "hello",
+					Password: "world",
+				},
+			},
+			serverEnableTCP: true,
+			serverEnableUDP: false,
+			checkServerErr: func(t *testing.T, err error) {
+				if err != errZeroULEN {
+					t.Errorf("err = %v, want %v", err, errZeroULEN)
+				}
+			},
+		},
+		{
+			name: "ZeroPLEN",
+			clientMsgs: [][]byte{
+				{Version, 1, MethodUsernamePassword},
+				{UsernamePasswordAuthVersion, 5, 'h', 'e', 'l', 'l', 'o', 0},
+			},
+			expectedResponses: [][]byte{
+				{Version, MethodUsernamePassword},
+			},
+			serverUserInfoByUsername: map[string]UserInfo{
+				"hello": {
+					Username: "hello",
+					Password: "world",
+				},
+			},
+			serverEnableTCP: true,
+			serverEnableUDP: false,
+			checkServerErr: func(t *testing.T, err error) {
+				if err != errZeroPLEN {
+					t.Errorf("err = %v, want %v", err, errZeroPLEN)
+				}
+			},
+		},
+		{
+			name: "IncorrectUsername",
+			clientMsgs: [][]byte{
+				{Version, 1, MethodUsernamePassword},
+				{UsernamePasswordAuthVersion, 5, 'h', 'e', 'l', 'l', 'o', 5, 'w', 'o', 'r', 'l', 'd'},
+			},
+			expectedResponses: [][]byte{
+				{Version, MethodUsernamePassword},
+				{UsernamePasswordAuthVersion, 1},
+			},
+			serverUserInfoByUsername: map[string]UserInfo{
+				"he11o": {
+					Username: "he11o",
+					Password: "world",
+				},
+			},
+			serverEnableTCP: true,
+			serverEnableUDP: false,
+			checkServerErr: func(t *testing.T, err error) {
+				if err != ErrIncorrectUsernamePassword {
+					t.Errorf("err = %v, want %v", err, ErrIncorrectUsernamePassword)
+				}
+			},
+		},
+		{
+			name: "IncorrectPassword",
+			clientMsgs: [][]byte{
+				{Version, 1, MethodUsernamePassword},
+				{UsernamePasswordAuthVersion, 5, 'h', 'e', 'l', 'l', 'o', 5, 'w', 'o', 'r', 'l', 'd'},
+			},
+			expectedResponses: [][]byte{
+				{Version, MethodUsernamePassword},
+				{UsernamePasswordAuthVersion, 1},
+			},
+			serverUserInfoByUsername: map[string]UserInfo{
+				"hello": {
+					Username: "hello",
+					Password: "wor1d",
+				},
+			},
+			serverEnableTCP: true,
+			serverEnableUDP: false,
+			checkServerErr: func(t *testing.T, err error) {
+				if err != ErrIncorrectUsernamePassword {
+					t.Errorf("err = %v, want %v", err, ErrIncorrectUsernamePassword)
+				}
+			},
+		},
+		{
+			name: "UnsupportedCommand/Connect",
+			clientMsgs: [][]byte{
+				{Version, 1, MethodNoAuthenticationRequired},
+				{Version, CmdConnect, 0, AtypIPv4, 127, 0, 0, 1, 0, 80},
+			},
+			expectedResponses: [][]byte{
+				{Version, MethodNoAuthenticationRequired},
+				{Version, ReplyCommandNotSupported, 0, AtypIPv4, 0, 0, 0, 0, 0, 0},
+			},
+			serverUserInfoByUsername: nil,
+			serverEnableTCP:          false,
+			serverEnableUDP:          true,
+			checkServerErr: func(t *testing.T, err error) {
+				var e UnsupportedCommandError
+				if !errors.As(err, &e) {
+					t.Fatalf("err = %v, want %T", err, e)
+				}
+				if e != CmdConnect {
+					t.Errorf("e = %d, want %d", e, CmdConnect)
+				}
+			},
+		},
+		{
+			name: "UnsupportedCommand/Bind",
+			clientMsgs: [][]byte{
+				{Version, 1, MethodNoAuthenticationRequired},
+				{Version, CmdBind, 0, AtypIPv4, 0, 0, 0, 0, 0, 0},
+			},
+			expectedResponses: [][]byte{
+				{Version, MethodNoAuthenticationRequired},
+				{Version, ReplyCommandNotSupported, 0, AtypIPv4, 0, 0, 0, 0, 0, 0},
+			},
+			serverUserInfoByUsername: nil,
+			serverEnableTCP:          true,
+			serverEnableUDP:          true,
+			checkServerErr: func(t *testing.T, err error) {
+				var e UnsupportedCommandError
+				if !errors.As(err, &e) {
+					t.Fatalf("err = %v, want %T", err, e)
+				}
+				if e != CmdBind {
+					t.Errorf("e = %d, want %d", e, CmdBind)
+				}
+			},
+		},
+		{
+			name: "UnsupportedCommand/UDPAssociate",
+			clientMsgs: [][]byte{
+				{Version, 1, MethodNoAuthenticationRequired},
+				{Version, CmdUDPAssociate, 0, AtypIPv4, 0, 0, 0, 0, 0, 0},
+			},
+			expectedResponses: [][]byte{
+				{Version, MethodNoAuthenticationRequired},
+				{Version, ReplyCommandNotSupported, 0, AtypIPv4, 0, 0, 0, 0, 0, 0},
+			},
+			serverUserInfoByUsername: nil,
+			serverEnableTCP:          true,
+			serverEnableUDP:          false,
+			checkServerErr: func(t *testing.T, err error) {
+				var e UnsupportedCommandError
+				if !errors.As(err, &e) {
+					t.Fatalf("err = %v, want %T", err, e)
+				}
+				if e != CmdUDPAssociate {
+					t.Errorf("e = %d, want %d", e, CmdUDPAssociate)
+				}
+			},
+		},
+		{
+			name: "AcceptRequiresTCPConn",
+			clientMsgs: [][]byte{
+				{Version, 1, MethodNoAuthenticationRequired},
+				{Version, CmdUDPAssociate, 0, AtypIPv4, 0, 0, 0, 0, 0, 0},
+			},
+			expectedResponses: [][]byte{
+				{Version, MethodNoAuthenticationRequired},
+			},
+			serverUserInfoByUsername: nil,
+			serverEnableTCP:          false,
+			serverEnableUDP:          true,
+			checkServerErr: func(t *testing.T, err error) {
+				if err != zerocopy.ErrAcceptRequiresTCPConn {
+					t.Errorf("err = %v, want %v", err, zerocopy.ErrAcceptRequiresTCPConn)
+				}
+			},
+		},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			testStreamServerError(t, c.clientMsgs, c.expectedResponses, c.serverUserInfoByUsername, c.serverEnableTCP, c.serverEnableUDP, c.checkServerErr)
+		})
+	}
+}
+
+func testStreamServerError(
+	t *testing.T,
+	clientMsgs [][]byte,
+	expectedResponses [][]byte,
+	serverUserInfoByUsername map[string]UserInfo,
+	serverEnableTCP, serverEnableUDP bool,
+	checkServerErr func(*testing.T, error),
+) {
+	pl, pr := pipe.NewDuplexPipe()
+
+	var (
+		wg         sync.WaitGroup
+		cerr, serr error
+	)
+
+	wg.Add(3)
+
+	go func() {
+		defer func() {
+			_ = pl.CloseWrite()
+			wg.Done()
+		}()
+
+		for _, msg := range clientMsgs {
+			if _, cerr = pl.Write(msg); cerr != nil {
+				return
+			}
+		}
+	}()
+
+	go func() {
+		defer func() {
+			_ = pl.CloseRead()
+			wg.Done()
+		}()
+
+		b := make([]byte, 1024)
+
+		for _, expectedResp := range expectedResponses {
+			n, err := pl.Read(b)
+			if err != nil {
+				t.Errorf("Failed to read response: %v", err)
+				return
+			}
+
+			resp := b[:n]
+			if !bytes.Equal(resp, expectedResp) {
+				t.Errorf("resp = %v, want %v", resp, expectedResp)
+			}
+		}
+	}()
+
+	go func() {
+		defer func() {
+			_ = pr.Close()
+			wg.Done()
+		}()
+
+		if serverUserInfoByUsername == nil {
+			_, serr = ServerAccept(pr, serverEnableTCP, serverEnableUDP)
+		} else {
+			_, _, serr = ServerAcceptUsernamePassword(pr, serverUserInfoByUsername, serverEnableTCP, serverEnableUDP)
+		}
+	}()
+
+	wg.Wait()
+
+	if cerr != nil {
+		t.Fatalf("cerr = %v", cerr)
+	}
+	checkServerErr(t, serr)
+}
