f21b20cca52272fded38e6546f06d1403a6d5438
database64128 <free122448@hotmail.com>
2025-03-23 01:27:03 +0800

    🐦 netio: add TCPClientConfig

--- netio/tcp.go
+++ netio/tcp.go
@@ -6,6 +6,31 @@ import (
 	"github.com/database64128/shadowsocks-go/conn"
 )
 
+// TCPClientConfig is the configuration for a TCP client.
+type TCPClientConfig struct {
+	// Name is the name of the client.
+	Name string
+
+	// Network controls the address family when resolving domain name destination addresses.
+	//
+	// - "tcp": System default, likely dual-stack.
+	// - "tcp4": Resolve to IPv4 addresses.
+	// - "tcp6": Resolve to IPv6 addresses.
+	Network string
+
+	// Dialer is the dialer used to establish connections.
+	Dialer conn.Dialer
+}
+
+// NewTCPClient returns a new TCP client.
+func (c *TCPClientConfig) NewTCPClient() *TCPClient {
+	return &TCPClient{
+		name:    c.Name,
+		network: c.Network,
+		dialer:  c.Dialer,
+	}
+}
+
 // TCPClient opens TCP connections and returns them directly.
 //
 // TCPClient implements [StreamClient] and [StreamDialer].
@@ -19,15 +44,6 @@ var (
 	_ StreamClient = (*TCPClient)(nil)
 	_ StreamDialer = (*TCPClient)(nil)
 )
-
-// NewTCPClient returns a new TCP client.
-func NewTCPClient(name, network string, dialer conn.Dialer) *TCPClient {
-	return &TCPClient{
-		name:    name,
-		network: network,
-		dialer:  dialer,
-	}
-}
 
 // NewStreamDialer implements [StreamClient.NewStreamDialer].
 func (c *TCPClient) NewStreamDialer() (StreamDialer, StreamDialerInfo) {
